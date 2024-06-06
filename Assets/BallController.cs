4b9b0a92e11153245d5a302bd791e54527262c5b
nakanaka3 <160567129+nakanaka3@users.noreply.github.com>
2024-06-06 17:37:17 +0900

    BallController
    ボタンにタグをつけて、ボールが番号を認識して音と色を変える

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,31 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class BallController : MonoBehaviour
 {
     // 次に押すべきボタンの番号
     private int nextButtonNumber = 1;
-
+    public AudioSource audioSource;
+    public AudioClip buttonSound;
     void OnCollisionEnter(Collision collision)
     {
         // 衝突したオブジェクトのタグを取得
         string currentTag = collision.gameObject.tag;
-
         // タグが次に押すべきボタンのタグと一致するかチェック
         if (currentTag == "Button" + nextButtonNumber)
         {
+            // 一致したら、音を鳴らす
+            audioSource.PlayOneShot(buttonSound);
+
+            // Change the color of the button to show that it has been pressed
+            Renderer renderer = collision.gameObject.GetComponent<Renderer>();
+            renderer.material.color = Color.blue;
+
             // 一致したら、次に押すべきボタンの番号を更新
             nextButtonNumber++;
-
             // 全てのボタンを押し終わったら、次の番号を1にリセット
             if (nextButtonNumber > 6)
             {
                 nextButtonNumber = 1;
             }
-
             // ボタンを押したことを表示（デバッグ用）
             Debug.Log("Pressed button: " + currentTag);
         }
     }
 }
+
+
+
--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -4,37 +4,4 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
-    public Vector3 pressedScale = new Vector3(1, 0.5f, 1);
-    private Vector3 originalScale;
-    public AudioSource audioSource;
-    public AudioClip buttonSound;
-
-    private void Start()
-    {
-        originalScale = transform.localScale;
-
-        // ボタンの色を薄いグレーに設定
-        GetComponent<Renderer>().material.color = new Color(0.75f, 0.75f, 0.75f);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ball"))
-        {
-            transform.localScale = pressedScale;
-
-            audioSource.PlayOneShot(buttonSound);
-
-            // ボタンの色を濃いグレーに変更
-            GetComponent<Renderer>().material.color = new Color(0.33f, 0.33f, 0.33f);
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ball"))
-        {
-            transform.localScale = originalScale;
-        }
-    }
 }
